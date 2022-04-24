1375cce1245a53ef87657e502ca05dbb8192b113
Rei-x <wszyzdgo@gmail.com>
2022-04-24 15:48:56 +0000

    Fix repo root issue

--- .gitpod/addOrigin.sh
+++ .gitpod/addOrigin.sh
@@ -1,6 +1,6 @@
 url=`gp url 3000`
 hostname="${url:8}"
-cd /workspace/appwrite/appwrite
+cd $GITPOD_REPO_ROOT/appwrite
 docker-compose exec -e hostname=$hostname -T mariadb sh -c 'exec mysql -uroot -p"rootsecretpassword" appwrite -e "INSERT INTO _console_platforms VALUES (2,'"'6260031cb459b83f1490'"','"'6260030f877ddd7cb032'"','"'web'"','"'web'"','"''"','"''"','"'$hostname'"',1650459420,1650459420)"'
 docker-compose exec -T mariadb sh -c 'exec mysql -uroot -p"rootsecretpassword" appwrite -e "INSERT INTO _console_platforms_perms VALUES (3,'"'read'"','"'role:all'"','"'6260031cb459b83f1490'"'), (4,'"'write'"','"'role:all'"','"'6260031cb459b83f1490'"');"'
 docker-compose exec -T redis redis-cli del cache-_console:projects:6260030f877ddd7cb032
--- .gitpod/updateEndpointUrl.sh
+++ .gitpod/updateEndpointUrl.sh
@@ -1,2 +1,2 @@
 endpointUrl=`gp url 8080`/v1
-sed -i -E "s|(NEXT_PUBLIC_ENDPOINT=).*|\1$endpointUrl|" /workspace/appwrite/.env.local
+sed -i -E "s|(NEXT_PUBLIC_ENDPOINT=).*|\1$endpointUrl|" $GITPOD_REPO_ROOT/.env.local
